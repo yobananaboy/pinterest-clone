e4ad29e19582d8c123682f06cb1d69dd43b7cfdb
Matt <mattkeegan@btinternet.com>
2018-02-24 12:29:43 +0000

    Clear error message on successful post

--- client/app/component/Posts/AddPost.js
+++ client/app/component/Posts/AddPost.js
@@ -18,7 +18,7 @@ class AddPost extends Component {
     }
     
     componentDidMount() {
-        this.props.sendNewPostHasErrorredMessage("");
+        this.props.sendNewPostHasErrorredMessage(null);
     }
 
     handleChange(e) {
@@ -54,7 +54,7 @@ class AddPost extends Component {
         // test image url
         this.testImage(this.state.url, (testURL, result) => {
             if (result == "success") {
-                this.props.sendNewPostHasErrorredMessage("");
+                this.props.sendNewPostHasErrorredMessage(null);
                 // you can submit the form now
                 // add new post, sending user and info about post
                 this.props.addNewPost(this.props.user, {
