9b92c2bd8e94588ea17f324277b5bd53da53d3c0
Matt <mattkeegan@btinternet.com>
2018-02-27 13:52:38 +0000

    Update link to mattkeegan.uk

--- client/app/component/Footer.js
+++ client/app/component/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 export const Footer = () => {
   return(
         <div className="footer">
-            <h5 className='text-center' id='made-by'>Made by <a href='https://github.com/yobananaboy' target='_blank'>Matt K</a></h5>
+            <h5 className='text-center' id='made-by'>Made by <a href='https://mattkeegan.uk'>Matt K</a></h5>
         </div>
       );
 };
